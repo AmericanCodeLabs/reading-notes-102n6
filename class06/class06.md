728768241a3812a278ecb0a1f40fa328daf6ed43
khoitq-acl <khoitq@americancodelab.com>
2023-12-01 21:44:40 +0700

    Add class 05

---
+++ class06/class06.md
@@ -0,0 +1,13 @@
+# Class 06: CSS & JS
+
+## CSS hover
+
+## JS basic
+
+### alert()
+
+- dùng để hiển thị một hộp thoại trên màn hình người dùng
+
+```js
+alert("$Message")
+```
---
+++ class06/lab/main.js
@@ -0,0 +1,42 @@
+// alert("Hello Hưng");
+
+// console.log
+// dùng để in ra nội dung trong console devtool
+
+// console.log("Hello Khôi")
+
+// alert(prompt("What is your name?"))
+
+// document.write("Hello Hưng")
+
+// document.write(prompt("Tên bạn là gì?"));
+
+// variables
+// Biến là một container để chứa dữ liệu lúc chương trình đang chạy
+
+// 4 cách khai báo biến
+
+// var hungName = 'Hưng';
+// console.log(hungName);
+
+// today = 'Friday';
+// console.log(today);
+
+// // ưu tiên
+// let khoiName = 'Khôi';
+// console.log(khoiName);
+
+// khoiName = 'Khôi 1';
+// console.log(khoiName);
+
+// const className = '102n6';
+// console.log(className);
+
+let userFullname = prompt("What is your name");
+
+
+// đề bài nhập tên người dùng và chào tên người đó
+
+console.log("Xin chào: " + userFullname);
+
+
